19f0d54855cb8e8479217e40eb75cdce60d334e2
geshengpsn <gesheng199832@126.com>
2024-04-04 02:07:05 +0800

    Update rigid and rotation examples, display it not debug it

--- examples/rigid.rs
+++ examples/rigid.rs
@@ -1,14 +1,16 @@
+use std::f32::consts::FRAC_PI_2;
+
 use liealg::prelude::*;
 use liealg::Vec6;
 
 fn main() {
-    let vec = Vec6::new([0., 0., 1.], [0., -1., 0.]);
-    println!("vec: {:?}", vec);
+    let vec = Vec6::new([0., 0., 1.], [0., -1., 0.]) * FRAC_PI_2;
+    println!("vec: {}", vec);
     let se3 = vec.hat();
-    println!("se3: {:?}", se3);
+    println!("se3: {}", se3);
     let rigid = se3.exp();
-    println!("rigid: {:?}", rigid);
+    println!("rigid: {:.2}", rigid);
     let se3_ = rigid.log();
     let vec_ = se3_.vee();
-    println!("vec_: {:?}", vec_);
+    println!("vec_: {}", vec_);
 }
