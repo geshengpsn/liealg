c06f3b35375e4f8358c4d9e474259a0f99350934
geshengpsn <gesheng199832@126.com>
2024-04-04 03:17:55 +0800

    impl display for point

--- src/point.rs
+++ src/point.rs
@@ -1,9 +1,22 @@
+use core::fmt::Display;
+
 use nalgebra::Vector3;
+
+use crate::Real;
 
 /// point in 3D space
 #[derive(Debug)]
 pub struct Point<T> {
     pub(crate) val: Vector3<T>,
+}
+
+impl<T> Display for Point<T>
+where
+    T: Display + Real,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.val.fmt(f)
+    }
 }
 
 impl<T> Point<T> {
